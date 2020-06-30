8c2d9cf91d2a1e9966e83ac688b4f65ca0c1893f
KaChiLau <tencchi@hotmail.com>
2020-06-30 13:32:16 +0900

    Updates

---
+++ precache-manifest.7c27827ec04d591388bc26d482c50b30.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "696f0ac4285eb35fd41e953b81440cc7",
+    "url": "/MyProjectPage/index.html"
+  },
+  {
+    "revision": "3979b10235b188c42e10",
+    "url": "/MyProjectPage/static/css/main.d9fd1ba6.chunk.css"
+  },
+  {
+    "revision": "67514a0fbcac32273a10",
+    "url": "/MyProjectPage/static/js/2.cc597e50.chunk.js"
+  },
+  {
+    "revision": "3979b10235b188c42e10",
+    "url": "/MyProjectPage/static/js/main.9d10a14a.chunk.js"
+  },
+  {
+    "revision": "8c988c57971979321133",
+    "url": "/MyProjectPage/static/js/runtime~main.59189e81.js"
+  }
+]);
